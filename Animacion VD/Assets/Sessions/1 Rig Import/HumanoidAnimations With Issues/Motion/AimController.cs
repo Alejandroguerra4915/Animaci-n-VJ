aa07d4436ad5812cd61fcc8f3e8573e22e77c3a0
Phentecost12 <93055187+Phentecost12@users.noreply.github.com>
2024-02-26 19:20:16 -0500

    si

--- Animacion VD/Assets/Sessions/1 Rig Import/HumanoidAnimations With Issues/Motion/AimController.cs
+++ Animacion VD/Assets/Sessions/1 Rig Import/HumanoidAnimations With Issues/Motion/AimController.cs
@@ -5,15 +5,36 @@ using ThirdPersonShooter;
 using UnityEngine.InputSystem;
 using UnityEngine.UIElements;
 
+[RequireComponent(typeof(Animator))]
+
 public class AimController : ThirdPersonShooterPlayerScript
 {
     [SerializeField] private Transform cameraRotationTarget;
+    [SerializeField] private float lookSpeed = 30;
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
 
     public void Look(InputAction.CallbackContext context)
     {
         Vector2 inputValue = context.ReadValue<Vector2>();
         if (cameraRotationTarget == null) return;
 
-        cameraRotationTarget.RotateAround(transform.position, transform.up, inputValue.x);
+        cameraRotationTarget.RotateAround(transform.position, transform.up, inputValue.x * lookSpeed * Time.deltaTime);
     }
+
+    public void ToogleAim(InputAction.CallbackContext context) 
+    {
+        float inputValues = context.ReadValue<float>();
+        animator.SetBool("ToogleAim", inputValues>0);
+        //playerData.State = inputValues > 0 ? ThirdPersonShooterPlayerData.PlayerState.AimingMode : ThirdPersonShooterPlayerData.PlayerState.NormalMode;
+    }
+
+    //protected override void OnStateChanged(ThirdPersonShooterPlayerData.PlayerState state)
+    //{
+    //    //animator.SetBool("ToogleAim", state == ThirdPersonShooterPlayerData.PlayerState.AimingMode);
+    //}
 }
